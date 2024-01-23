6134891961642b0dfad5d5229c2ed47216b273ca
Muhammad-Naveed704 <107260698+Muhammad-Naveed704@users.noreply.github.com>
2024-01-24 02:39:53 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-    apiKey: "AIzaSyCHVuImeCQ1dn-wsn6JjP5WT_CvNOpyG_o",
-    authDomain: "netflix0-fc9f3.firebaseapp.com",
-    projectId: "netflix0-fc9f3",
-    storageBucket: "netflix0-fc9f3.appspot.com",
-    messagingSenderId: "494338820292",
-    appId: "1:494338820292:web:241e30e6741c83b8916191"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
  
 const app = initializeApp(firebaseConfig);
